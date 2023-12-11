97f8161169cd1677ed8e20b5b7fe589cae8b2be6
Andres Santamarina <andresesantamarina@gmail.com>
2023-12-11 11:07:33 -0300

    agregamos 2 props en TituloSecundario

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import TituloSecundario from "./components/TituloSecundario";
 
 function App() {
   //aqui agregamos toda la logica que necesite el componente
+  const anioActual = 2023;
   return (
     //aqui puedo agregar un poquito mas de logica
     <>
       {/* aqui empiezo a maquetar */}
       <section className="container my-4">
         <h1 className="display-3 text-center">Contador con React</h1>
-        <TituloSecundario />
+        <TituloSecundario comision="c73i" anioActualProps={anioActual} />
       </section>
     </>
   );
--- src/components/TituloSecundario.jsx
+++ src/components/TituloSecundario.jsx
@@ -1,10 +1,20 @@
-const TituloSecundario = () => {
-    return (
-        <article>
-            <h2>Práctica con react</h2>
-            <p>React es una biblioteca de JavaScript desarrollada por Facebook para construir interfaces de usuario interactivas y eficientes en aplicaciones web de una sola página. Su enfoque declarativo y su arquitectura basada en componentes facilitan la creación de interfaces reutilizables y escalables. React ha ganado popularidad debido a su rendimiento optimizado y su capacidad para trabajar de manera integrada con otras tecnologías, convirtiéndolo en una herramienta fundamental en el desarrollo moderno de aplicaciones web.</p>
-        </article>
-    );
+const TituloSecundario = (props) => {
+  return (
+    <article>
+      <h2>Práctica con react {props.anioActualProps}</h2>
+      <p>
+        React es una biblioteca de JavaScript desarrollada por Facebook para
+        construir interfaces de usuario interactivas y eficientes en
+        aplicaciones web de una sola página. Su enfoque declarativo y su
+        arquitectura basada en componentes facilitan la creación de interfaces
+        reutilizables y escalables. React ha ganado popularidad debido a su
+        rendimiento optimizado y su capacidad para trabajar de manera integrada
+        con otras tecnologías, convirtiéndolo en una herramienta fundamental en
+        el desarrollo moderno de aplicaciones web. <br />
+        Comisión: {props.comision}
+      </p>
+    </article>
+  );
 };
 
 export default TituloSecundario;
