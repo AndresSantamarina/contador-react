e193d9b8887a474dd5a207cdb2b53337c27da29d
Andres Santamarina <andresesantamarina@gmail.com>
2023-12-11 11:32:14 -0300

    agregamos una variable numero y una funcion anonima para aumentar el contador

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,13 +1,15 @@
 const Contador = () => {
-    
+  let numero = 10;
 
-    return (
-        <article className="text-center">
-            <h2>Contador</h2>
-            <h3>10</h3>
-            <button className="btn btn-outline-success">+1</button>
-        </article>
-    );
+  return (
+    <article className="text-center">
+      <h2>Contador</h2>
+      <h3>{numero}</h3>
+      <button className="btn btn-outline-success" onClick={() => numero++}>
+        +1
+      </button>
+    </article>
+  );
 };
 
 export default Contador;
