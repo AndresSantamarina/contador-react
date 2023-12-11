c8082dab9a609ce8709ef71c8ec89b4724da3d66
Andres Santamarina <andresesantamarina@gmail.com>
2023-12-11 11:24:12 -0300

    agregamos el maquetado del componente Contador

---
+++ src/components/Contador.jsx
@@ -0,0 +1,13 @@
+const Contador = () => {
+    
+
+    return (
+        <article className="text-center">
+            <h2>Contador</h2>
+            <h3>10</h3>
+            <button className="btn btn-outline-success">+1</button>
+        </article>
+    );
+};
+
+export default Contador;
